16195df1b7f3fc20036f314270508ca68af1178e
Paulo Varassin <pauloarthurvm@hotmail.com>
2025-04-07 13:59:30 -0300

    First Commit

---
+++ src/main/java/com/designpatterns/gof/LabDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.designpatterns.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designpatterns/gof/LabDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designpatterns.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
